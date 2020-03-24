7bb7c77a0cad5439f62500e75e9eb6cb2c1fff95
johnj974 <jjdug@hotmail.com>
2020-03-24 18:52:28 +0000

    base template added for login/logout page

--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render, reverse, redirect
 from django.contrib import auth, messages
 # form imported from forms.py file
 from accounts.forms import loginForm
+# additional library to stop unauthorised use of functions
+from django.contrib.auth.decorators import login_required
 
 
 # function to render index page
@@ -12,6 +14,7 @@ def index(request):
 
 
 # function to activate logout/logout user
+@login_required
 def logout(request):
     auth.logout(request)
     messages.success(request, "Logout Successfull")
@@ -20,6 +23,8 @@ def logout(request):
 
 # login function with django forms
 def login(request):
+    if request.user.is_authenticated:
+        return redirect(reverse('index'))
     if request.method == 'POST':
         login_form = loginForm(request.POST)
         if login_form.is_valid():
@@ -28,6 +33,7 @@ def login(request):
             if user:
                 auth.login(user=user, request=request)
                 messages.success(request, "You have successfully logged in!")
+                return redirect(reverse('index'))
             else:
                 login_form.add_error(None, "Your username or password is incorrect")
     else:
