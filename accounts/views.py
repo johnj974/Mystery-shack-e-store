ad7efddaf27ac9a3cec53de370b7fa452d8416d2
johnj974 <jjdug@hotmail.com>
2020-03-24 14:53:00 +0000

    still working on logout function

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,7 @@
 # reverse, redirect added to existing library
 from django.shortcuts import render, reverse, redirect
 # additional django library
-from django.contrib import auth
+from django.contrib import auth, messages
 
 
 # function to render index page
@@ -12,5 +12,6 @@ def index(request):
 # function to activate logout/logout user
 def logout(request):
     auth.logout(request)
+    messages.success(request, "Logout Successfull")
     return redirect(reverse('index'))
 
