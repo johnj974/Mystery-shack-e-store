a03a889412d1898e8360be130235013e12f398af
johnj974 <jjdug@hotmail.com>
2020-03-24 14:33:23 +0000

    logout function added

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
+# reverse, redirect added to existing library
+from django.shortcuts import render, reverse, redirect
+# additional django library
+from django.contrib import auth
 
 
-# function request to render index page
+# function to render index page
 def index(request):
     return render(request, 'index.html')
+
+
+# function to activate logout/logout user
+def logout(request):
+    auth.logout(request)
+    return redirect(reverse('index'))
+
