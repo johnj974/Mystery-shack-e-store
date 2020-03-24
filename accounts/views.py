928a5e0841f24916500a6b4827f5417bf8b32930
johnj974 <jjdug@hotmail.com>
2020-03-24 16:13:32 +0000

    working on login function

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,8 @@
 from django.shortcuts import render, reverse, redirect
 # additional django library
 from django.contrib import auth, messages
+# form imported from forms.py file
+from accounts.forms import loginForm
 
 
 # function to render index page
@@ -14,4 +16,21 @@ def logout(request):
     auth.logout(request)
     messages.success(request, "Logout Successfull")
     return redirect(reverse('index'))
+
+
+# login function with django forms
+def login(request):
+    if request.method == 'POST':
+        login_form = loginForm(request.POST)
+        if login_form.is_valid():
+            user = auth.authenticate(username=request.POST['username'],
+                                     password=request.POST['password'])
+            if user:
+                auth.login(user=user, request=request)
+                messages.success(request, "You have successfully logged in!")
+            else:
+                login_form.add_error(None, "Your username or password is incorrect")
+    else:
+        login_form = loginForm()
+    return render(request, 'login.html', {'login_form': login_form})    # form passed in for rendering to page
 
