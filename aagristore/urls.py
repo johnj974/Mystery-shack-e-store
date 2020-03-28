fc06f2929f3e5449871f8a1362edf049c9dfcd9f
johnj974 <jjdug@hotmail.com>
2020-03-28 17:42:38 +0000

    changed the home page/ added media path to url patterns

--- aagristore/urls.py
+++ aagristore/urls.py
@@ -22,7 +22,7 @@ from accounts.views import index, logout, login, registration, user_profile
 # imported urls from products app
 from products import urls as urls_products
 # import the view function
-from products.views import all_machinery
+from products.views import all_machinery, Machinery
 # import the media root from settings
 from .settings import MEDIA_ROOT
 
@@ -34,4 +34,5 @@ urlpatterns = [
     url(r'^accounts/register/$', registration, name='registration'),
     url(r'^accounts/profile/$', user_profile, name='profile'),
     url(r'^products/', include(urls_products)),
+    url(r'^media/(?P<path>.*)$', static.serve, {'document_root': MEDIA_ROOT}),
 ]
