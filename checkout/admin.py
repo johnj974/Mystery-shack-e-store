e79a55f5698c0e3ddc757bb9f150be085a44a741
johnj974 <jjdug@hotmail.com>
2020-03-31 09:28:41 +0000

    working on checkout/created customer model

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Order, OrderItem
 
-# Register your models here.
+
+class OrderItemAdminInline(admin.TabularInline):
+    model = OrderItem
+
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (OrderItemAdminInline,)
+
+
+admin.site.register(Order, OrderAdmin)
--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from products.models import Machinery
 
-# Create your models here.
+
+# object for storing customer order details
+class Order(models.Model):
+    full_name = models.CharField(max_length=50, blank=False)
+    address1 = models.CharField(max_length=50, blank=False)
+    address2 = models.CharField(max_length=50, blank=True)
+    town_or_city = models.CharField(max_length=20, blank=False)
+    county = models.CharField(max_length=20, blank=False)
+    country = models.CharField(max_length=30, blank=False)
+    contact_number = models.IntegerField(blank=False)
+    date = models.DateField()
+
+    def __str__(self):
+        return "{0}-{1}-{2}".format(self.id, self.date, self.full_name)
+
+
+# object for item ordered
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, null=False)
+    product = models.ForeignKey(Machinery, null=False)  # Watch here product
+    quantity = models.IntegerField(blank=False)
+
+    def __str__(self):
+        return "{0} {1} @ {2}".format(self.quantity, self.product.name, self.product.price)
+
+
