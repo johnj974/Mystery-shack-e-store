d3bb1f489b2b5685d0bb8d4a05534ac30c3e00e2
johnj974 <jjdug@hotmail.com>
2020-04-02 15:21:59 +0000

    stripe not working, save before fix

--- checkout/forms.py
+++ checkout/forms.py
@@ -5,8 +5,8 @@ from .models import Order
 # payment form for customer
 class PaymentForm(forms.Form):
     MONTH_CHOICES = [(m, m) for m in range(1, 12)]
-    YEAR_CHOICES = [(y, y) for y in range(2020, 2099)]
-    card_number = forms.CharField(label='Card number', required=False)
+    YEAR_CHOICES = [(y, y) for y in range(2020, 2030)]
+    credit_card_number = forms.CharField(label='Card number', required=False)
     cvv = forms.CharField(label='Security code', required=False)
     expiry_month = forms.ChoiceField(label='Month', choices=MONTH_CHOICES, required=False)
     expiry_year = forms.ChoiceField(label='Year', choices=YEAR_CHOICES, required=False)
