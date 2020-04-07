d4da01c765fc8b036046dc4107c0f56b147c9f96
johnj974 <jjdug@hotmail.com>
2020-04-07 16:03:21 +0000

    serach function fixed/ missing % around url

--- products/models.py
+++ products/models.py
@@ -4,9 +4,9 @@ from django.db import models
 # tractor object
 class Product(models.Model):
     name = models.CharField(max_length=50)
-    manufacturer = models.CharField(max_length=50)
-    description = models.TextField()
     horsepower = models.CharField(max_length=50)
+    description = models.TextField()
+    manufacturer = models.CharField(max_length=50)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(upload_to='images')
 
