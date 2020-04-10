670022dd8b8b910dfb0dced81567b753a2415e37
johnj974 <jjdug@hotmail.com>
2020-04-10 13:12:56 +0000

    tring to add update capability to product

--- products/urls.py
+++ products/urls.py
@@ -1,9 +1,10 @@
 # import our base url pattern
 from django.conf.urls import url, include
 # import all_machinery function to be used in our url pattern
-from .views import all_products, createProduct
+from .views import all_products, createProduct, updateProduct
 
 urlpatterns = [
     url('^$', all_products, name='products'),
     url("^create_product/$", createProduct, name='create_product'),
+     url("^update_product/<str:pk>/$", updateProduct, name='update_product'),
 ]
--- products/views.py
+++ products/views.py
@@ -10,6 +10,7 @@ def all_products(request):
     return render(request, 'products.html', {'products': products})
 
 
+# create product view
 def createProduct(request):
     form = ProductForm()
     if request.method == "POST":
@@ -19,3 +20,9 @@ def createProduct(request):
             return redirect(reverse('products'))
     context = {'form': form}
     return render(request, "product_form.html", context)
+
+
+def updateProduct(request, pk):
+    form = ProductForm()
+    context = {'form': form}
+    return redirect(reverse("products"), context)
