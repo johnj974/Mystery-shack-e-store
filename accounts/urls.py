adf619a4711542b73890044a9c9435cac87997a5
johnj974 <jjdug@hotmail.com>
2020-04-11 13:43:26 +0000

    heroku test for password reset again again again

--- accounts/urls.py
+++ accounts/urls.py
@@ -11,13 +11,15 @@ urlpatterns = [
     url("^login/$", login, name="login"),
     url("^registration/$", registration, name="registration"),
     url("^user_profile/$", user_profile, name="user_profile"),
-    url(r"^reset_password/$", password_reset, name= "reset_password"),
-    url(r'^reset_password/done/$', password_reset_done, name= "password_reset_done"),
-    url(r'^reset_password/confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', password_reset_confirm, name= "password_reset_confirm"),
-    url(r'^reset_password/complete/$', password_reset_complete, name= "password_reset_complete"),
+    url(r'^password-reset/', include(url_reset)),
 ]
 
     #url('^reset_password/', auth_views.PasswordResetView.as_view(), name="reset_password"),
     #url('^reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
     #url('^reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
     #url('^reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
+
+    #url(r"^reset_password/$", password_reset, name= "reset_password"),
+    #url(r'^reset_password/done/$', password_reset_done, name= "password_reset_done"),
+    #url(r'^reset_password/confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', password_reset_confirm, name= "password_reset_confirm"),
+    #url(r'^reset_password/complete/$', password_reset_complete, name= "password_reset_complete"),
