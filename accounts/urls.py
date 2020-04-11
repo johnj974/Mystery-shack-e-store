7fceff922f9fca0a5799dbd66c7add777d9c9107
johnj974 <jjdug@hotmail.com>
2020-04-11 10:50:17 +0000

    trying differant paths to get password reset to work

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url, include
 from .views import index, logout, login, registration, user_profile
 from accounts import url_reset
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
@@ -9,5 +10,8 @@ urlpatterns = [
     url("^login/$", login, name="login"),
     url("^registration/$", registration, name="registration"),
     url("^user_profile/$", user_profile, name="user_profile"),
-    url('^password-reset/', include(url_reset))
+    url('^reset_password/', auth_views.PasswordResetView.as_view(), name="reset_password"),
+    url('^reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
+    url('^reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+    url('^reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
 ]
