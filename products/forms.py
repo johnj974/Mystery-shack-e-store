545d8f1c179ffc041cedaa8dfca33757d8ae6ded
johnj974 <jjdug@hotmail.com>
2020-07-20 15:16:44 +0000

    removed unused categories from product model

--- products/forms.py
+++ products/forms.py
@@ -5,6 +5,6 @@ from .models import Product
 class ProductForm(ModelForm):
     class Meta:
         model = Product
-        fields = ['name', 'description', 'price',]
+        fields = ['name', 'description', 'price', ]
 
 
--- products/models.py
+++ products/models.py
@@ -3,9 +3,7 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=50)
-    horsepower = models.CharField(max_length=50)
     description = models.TextField()
-    manufacturer = models.CharField(max_length=50)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(upload_to='images')
 
